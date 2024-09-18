ccea6628feb6ff13ef77fb51988bbf268d9f46da
Behzad Raeisifard <behzad29@gmail.com>
2024-09-18 13:19:58 -0400

    Enable hot reloading while deployed locally in Docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import UserList from '@/app/components/UserList';
 export default function HomePage() {
   return (
       <div>
-        <h1>Home Page: </h1>
+        <h1>Home Page! </h1>
         <UserList />
       </div>
   );
