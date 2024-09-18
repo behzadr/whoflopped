3707da0b05cb962246817a4ffa39af7e5748e173
Behzad Raeisifard <behzad29@gmail.com>
2024-09-18 13:01:59 -0400

    Fix deploying locally in Docker

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 
+COPY prisma ./prisma
+RUN npx prisma generate
+
 COPY . .
 EXPOSE 3000
 CMD npm run dev
--- src/app/components/UserList.tsx
+++ src/app/components/UserList.tsx
@@ -4,32 +4,37 @@ import { useEffect, useState } from 'react';
 
 type User = {
     id: number;
-    email: string; // Add other fields according to your schema
+    email: string;
 };
 
 export default function UserList() {
     const [users, setUsers] = useState<User[]>([]);
+    console.log("UserList users array1:", users);
 
     useEffect(() => {
+        console.log("UserList users array2:", users);
         const fetchUsers = async () => {
             try {
                 const response = await fetch('/api/users');
+                console.log('Response status:', response.status);
                 const data = await response.json();
+                console.log('Data:', data);
                 setUsers(data);
             } catch (error) {
                 console.error('Error fetching users:', error);
+                setUsers([]);
             }
         };
 
         fetchUsers();
     }, []);
-
+    console.log("UserList users array3:", users);
     return (
         <div>
             <h1>User List</h1>
             <ul>
                 {users.map((user) => (
-                    <li key={user.id}>{user.email}</li> // Adjust fields as per your schema
+                    <li key={user.id}>{user.email}</li>
                 ))}
             </ul>
         </div>
