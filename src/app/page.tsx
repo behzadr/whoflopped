c2b3ab434162176889dc23f19c26c47f4596500a
Behzad Raeisifard <behzad29@gmail.com>
2024-10-09 21:05:26 -0400

    Fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import SignInForm from "app/components/SignInForm";
 
 export default function LandingPage() {
